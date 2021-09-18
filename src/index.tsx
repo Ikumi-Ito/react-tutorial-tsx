971378c5e19323ed3407c5143454b1dab8897e1e
Ikumi Ito <ikumi.ito.p8@dc.tohoku.ac.jp>
2021-09-19 02:56:29 +0900

    hooksとtsxでチュートリアル

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,131 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+type SquareType = "X" | "O" | null;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+type SquareProps = {
+  value: SquareType;
+  onClick: () => void;
+};
+
+type BoardProps = {
+  squares: Array<SquareType>;
+  onClick: (i: number) => void;
+};
+
+type HistoryProps = {
+  squares: Array<SquareType>;
+};
+
+const Square = (props: SquareProps) => {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
+};
+
+const Board = (props: BoardProps) => {
+  const renderSquare = (i: number) => {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+  };
+  return (
+    <div>
+      {Array(3)
+        .fill(0)
+        .map((_, i) => {
+          return (
+            <div className="board-row" key={i}>
+              {Array(3)
+                .fill(0)
+                .map((_, j) => {
+                  return renderSquare(i * 3 + j);
+                })}
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+const Game = () => {
+  const [history, setHistory] = useState<Array<HistoryProps>>([
+    { squares: Array(9).fill(null) },
+  ]);
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [stepNumber, setStepNumber] = useState<number>(0);
+
+  const handleClick = (i: number) => {
+    const _history = history.slice(0, stepNumber + 1);
+    const current = _history[_history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(_history.concat([{ squares: squares }]));
+    setStepNumber(_history.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const _history = history;
+  const current = _history[stepNumber];
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  const moves = _history.map((board, move) => {
+    const desc = move ? "Go to move #" + move : "Let's start a new game";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+function calculateWinner(squares: Array<SquareType>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
